9dbde70338862bd3cbd56be78d74cfdeced526c5
Woshiajuana <zhigang.chen@owulia.com>
2017-05-12 17:11:10 +0800

    首页页面已完善

---
+++ src/components/my-header.vue
@@ -0,0 +1,43 @@
+<template>
+    <header class="header-wrap">
+        <div class="header-content">
+            <nav-bar></nav-bar>
+        </div>
+    </header>
+</template>
+<script>
+    import NavBar from './nav-bar.vue'
+    export default {
+        name: 'my-header',
+        components: {
+            NavBar
+        }
+    }
+</script>
+<style lang="scss">
+    @import "../assets/scss/define";
+    .header-wrap{
+        @extend %pf;
+        @extend %w100;
+        @extend %l0;
+        @extend %t0;
+        @extend %oh;
+        z-index: 1;
+        height: 60px;
+        background-color: #fff;
+    }
+    .header-content{
+        @extend %ma;
+        @extend %clearfix;
+        margin-top: 20px;
+    }
+    @media only screen and (min-width:1000px) {
+        .header-wrap{
+            height: 120px;
+        }
+        .header-content{
+            width: 1000px;
+            margin-top: 60px;
+        }
+    }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,154 @@
 <template>
     <div class="container-view">
-        <h1>首页</h1>
+        <article-list-item
+            v-for="(article_list_item,article_list_index) in article_list_arr"
+            :article_type="article_list_item.article_type"
+            :article_time="article_list_item.article_time"
+            :article_title="article_list_item.article_title"
+            :article_href="article_list_item.article_href"
+        ></article-list-item>
+        <!--分页组件-->
+        <div class="pagination-wrap">
+            <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page="1"
+                :page-sizes="[100, 200, 300, 400]"
+                :page-size="100"
+                layout="total, prev, pager, next, jumper"
+                :total="1000">
+            </el-pagination>
+        </div>
+        <!--/分页组件-->
     </div>
 </template>
 <script>
+    import ArticleListItem from '../../components/article-list-item.vue'
+    import Pagination from '../../components/pagination.vue'
     export default {
-        name: 'home'
+        name: 'home',
+        data () {
+            return {
+                article_list_arr: [
+                    {
+                        article_type: 'html',
+                        article_time: '2017-05-12 14:36',
+                        article_title: '如何有效的学习html',
+                        article_href: '#/article'
+                    },
+                    {
+                        article_type: 'html',
+                        article_time: '2017-05-12 14:36',
+                        article_title: '如何有效的学习html',
+                        article_href: '#/article'
+                    },
+                    {
+                        article_type: 'html',
+                        article_time: '2017-05-12 14:36',
+                        article_title: '如何有效的学习html',
+                        article_href: '#/article'
+                    },
+                    {
+                        article_type: 'html',
+                        article_time: '2017-05-12 14:36',
+                        article_title: '如何有效的学习html',
+                        article_href: '#/article'
+                    },
+                    {
+                        article_type: 'html',
+                        article_time: '2017-05-12 14:36',
+                        article_title: '如何有效的学习html',
+                        article_href: '#/article'
+                    },
+                    {
+                        article_type: 'html',
+                        article_time: '2017-05-12 14:36',
+                        article_title: '如何有效的学习html',
+                        article_href: '#/article'
+                    },
+                    {
+                        article_type: 'html',
+                        article_time: '2017-05-12 14:36',
+                        article_title: '如何有效的学习html',
+                        article_href: '#/article'
+                    },
+                    {
+                        article_type: 'html',
+                        article_time: '2017-05-12 14:36',
+                        article_title: '如何有效的学习html',
+                        article_href: '#/article'
+                    },
+                    {
+                        article_type: 'html',
+                        article_time: '2017-05-12 14:36',
+                        article_title: '如何有效的学习html',
+                        article_href: '#/article'
+                    },
+                    {
+                        article_type: 'html',
+                        article_time: '2017-05-12 14:36',
+                        article_title: '如何有效的学习html',
+                        article_href: '#/article'
+                    },
+                    {
+                        article_type: 'html',
+                        article_time: '2017-05-12 14:36',
+                        article_title: '如何有效的学习html',
+                        article_href: '#/article'
+                    },
+                    {
+                        article_type: 'html',
+                        article_time: '2017-05-12 14:36',
+                        article_title: '如何有效的学习html',
+                        article_href: '#/article'
+                    },
+                    {
+                        article_type: 'html',
+                        article_time: '2017-05-12 14:36',
+                        article_title: '如何有效的学习html',
+                        article_href: '#/article'
+                    },
+                    {
+                        article_type: 'html',
+                        article_time: '2017-05-12 14:36',
+                        article_title: '如何有效的学习html',
+                        article_href: '#/article'
+                    }
+                ],
+                page_total: 10,
+                page_current: 2
+            }
+        },
+        components: {
+            ArticleListItem,
+            Pagination
+        },
+        methods: {
+            handleSizeChange(val) {
+                console.log(`每页 ${val} 条`);
+            },
+            handleCurrentChange(val) {
+                console.log(`当前页: ${val}`);
+            }
+        }
     }
 </script>
+<style lang="scss">
+    @import "../../assets/scss/define";
+    .pagination-wrap{
+        @extend %tac;
+        padding: 30px 0;
+        .el-pager li.active {
+            border-color: #333;
+            background-color: #333;
+            @extend %cfff;
+            cursor: default;
+            &:hover{
+                @extend %cfff;
+            }
+        }
+        .el-pager li:hover {
+            @extend %c3;
+        }
+    }
+</style>
