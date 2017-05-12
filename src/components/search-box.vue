4f17836f349ed268554720e32ec583d5113dce0f
Woshiajuana <zhigang.chen@owulia.com>
2017-05-12 18:26:52 +0800

    搜索页面

---
+++ src/components/search-box.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="search-wrap">
+        <input type="text" placeholder="请输入检索关键词" class="search-input">
+        <i class="search-btn">
+            <svg slot="icon" class="search-btn-icon">
+                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#search-btn-icon"></use>
+            </svg>
+        </i>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'search'
+    }
+</script>
+<style lang="scss">
+    @import "../assets/scss/define";
+    .search-wrap{
+        @extend %pr;
+        @extend %df;
+        @extend %f14;
+        margin-left: 20px;
+        width: 360px;
+        height: 45px;
+        line-height: 45px;
+        padding-left: 15px;
+        border-bottom: 1px solid #ccc;
+        &:after,
+        &:before{
+            content: '';
+            @extend %pa;
+            @extend %b0;
+            width: 1px;
+            height: 5px;
+            background-color: #ccc;
+        }
+        &:after{
+            @extend %r0;
+        }
+        &:before{
+            @extend %l0;
+        }
+    }
+    .search-input{
+        @extend %df1;
+        height: 45px;
+        border: none;
+    }
+    .search-btn{
+        @extend %pr;
+        @extend %cp;
+        width: 45px;
+        height: 45px;
+        &:hover{
+            .search-btn-icon{
+                fill: #333;
+            }
+        }
+    }
+    .search-btn-icon{
+        @extend %pa;
+        @extend %t50;
+        @extend %l50;
+        width: 20px;
+        height: 20px;
+        margin-top: -10px;
+        margin-left: -10px;
+        fill: #999;
+        transition: fill .3s;
+    }
+</style>
--- src/views/search/Search.vue
+++ src/views/search/Search.vue
@@ -1,13 +1,6 @@
 <template>
     <div class="container-view">
-        <div class="search-wrap">
-            <input type="text" placeholder="请输入检索关键词" class="search-input">
-            <i class="search-btn">
-                <svg slot="icon" class="search-btn-icon">
-                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#search-btn-icon"></use>
-                </svg>
-            </i>
-        </div>
+        <search-box></search-box>
         <div class="result-wrap">
             <p class="result-prompt">搜索结果：</p>
             <article-list-item
@@ -36,6 +29,7 @@
 <script>
     import ArticleListItem from '../../components/article-list-item.vue'
     import Pagination from '../../components/pagination.vue'
+    import SearchBox from '../../components/search-box.vue'
     export default {
         name: 'search',
         data () {
@@ -132,7 +126,8 @@
         },
         components: {
             ArticleListItem,
-            Pagination
+            Pagination,
+            SearchBox
         },
         methods: {
             handleSizeChange(val) {
@@ -154,57 +149,5 @@
         @extend %c3;
         line-height: 30px;
     }
-    .search-wrap{
-        @extend %pr;
-        @extend %df;
-        @extend %f14;
-        margin-left: 20px;
-        width: 360px;
-        height: 45px;
-        line-height: 45px;
-        padding-left: 15px;
-        border-bottom: 1px solid #ccc;
-        &:after,
-        &:before{
-            content: '';
-            @extend %pa;
-            @extend %b0;
-            width: 1px;
-            height: 5px;
-            background-color: #ccc;
-        }
-        &:after{
-            @extend %r0;
-        }
-        &:before{
-            @extend %l0;
-        }
-    }
-    .search-input{
-        @extend %df1;
-        height: 45px;
-        border: none;
-    }
-    .search-btn{
-        @extend %pr;
-        @extend %cp;
-        width: 45px;
-        height: 45px;
-        &:hover{
-            .search-btn-icon{
-                fill: #333;
-            }
-        }
-    }
-    .search-btn-icon{
-        @extend %pa;
-        @extend %t50;
-        @extend %l50;
-        width: 20px;
-        height: 20px;
-        margin-top: -10px;
-        margin-left: -10px;
-        fill: #999;
-        transition: fill .3s;
-    }
+
 </style>
