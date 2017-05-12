6f8abc350ff16216d7407dfe2476eaa9fe1e30b7
Woshiajuana <zhigang.chen@owulia.com>
2017-05-12 14:09:30 +0800

    头部已完善

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
     <div id="app">
+
+        <!--头部-->
+        <my-header></my-header>
+        <!--/头部-->
+
         <!--主体内容视图-->
         <!--<transition :name="animateName">-->
             <!--<keep-alive>-->
@@ -7,7 +12,7 @@
             <!--</keep-alive>-->
         <!--</transition>-->
         <!--/主体内容视图-->
-        
+
         <!--svg-->
         <svg-template></svg-template>
         <!--/svg-->
@@ -16,10 +21,12 @@
 
 <script>
     import SvgTemplate from './components/svg-template.vue'
+    import MyHeader from './components/my-header.vue'
     export default {
         name: 'app',
         components: {
-            SvgTemplate
+            SvgTemplate,
+            MyHeader
         }
     }
 </script>
---
+++ src/components/nav-bar.vue
@@ -0,0 +1,78 @@
+<template>
+    <nav class="nav-wrap">
+        <router-link class="nav-item" to="/" exact>
+            <span class="nav-item-con">
+                <svg slot="icon" class="nav-item-icon">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#home-icon"></use>
+                </svg>
+                <span class="nav-text">首页</span>
+            </span>
+        </router-link>
+        <router-link class="nav-item" to="/search">
+            <span class="nav-item-con">
+                <svg slot="icon" class="nav-item-icon">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#search-icon"></use>
+                </svg>
+                <span class="nav-text">搜索</span>
+            </span>
+        </router-link>
+        <router-link class="nav-item" to="/article">
+            <span class="nav-item-con">
+                <svg slot="icon" class="nav-item-icon">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#article-icon"></use>
+                </svg>
+                <span class="nav-text">文章</span>
+            </span>
+        </router-link>
+        <router-link class="nav-item" to="/directory">
+            <span class="nav-item-con">
+                <svg slot="icon" class="nav-item-icon">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#directory-icon"></use>
+                </svg>
+                <span class="nav-text">目录</span>
+            </span>
+        </router-link>
+        <router-link class="nav-item" to="/other">
+            <span class="nav-item-con">
+                <svg slot="icon" class="nav-item-icon">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#other-icon"></use>
+                </svg>
+                <span class="nav-text">其它</span>
+            </span>
+        </router-link>
+    </nav>
+</template>
+<script>
+    export default {
+        name: 'nav-bar'
+    }
+</script>
+<style lang="scss">
+    @import "../assets/scss/define";
+    .nav-wrap{
+        @extend %fr;
+        @extend %pr;
+    }
+    .nav-item{
+        @extend %f14;
+        @extend %c6;
+        margin: 0 10px;
+        &.router-link-active{
+            @extend %fwb;
+            color: #000;
+            .nav-item-icon{
+                fill: #000;
+            }
+        }
+    }
+    .nav-item-icon{
+        @extend %vam;
+        width: 20px;
+        height: 20px;
+        fill: #999;
+    }
+    .nav-text{
+        @extend %vam;
+    }
+
+</style>
