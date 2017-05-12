945090122db09178ee62e29eff81819dbc2d2709
Woshiajuana <zhigang.chen@owulia.com>
2017-05-12 10:27:03 +0800

    添加重置相关

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import App from './App'
+import axios from 'axios'
+import ElementUI from 'element-ui'
 import router from './router'
+import store from './store/index.js'
+import 'element-ui/lib/theme-default/index.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
+Vue.use(ElementUI);
 
-/* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
+    store,
     template: '<App/>',
     components: { App }
 });
