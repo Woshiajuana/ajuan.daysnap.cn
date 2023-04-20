527e742a0b1cd5127f99542c089911176e51a280
chenzhigang <979703986@qq.com>
2023-04-20 22:27:46 +0800

    chore: next

--- blog/scripts/index.ts
+++ blog/scripts/index.ts
@@ -1,30 +1,43 @@
 import path from 'path'
-import fs from 'fs'
+import fs from 'fs-extra'
 import glob from 'fast-glob'
 import { normalizePath } from '@daysnap/utils'
 import matter from 'gray-matter'
 import { resolve } from './utils'
 
-async function run(input: string) {
+async function run(options: {
+  input: string
+  output: string
+}) {
+  const { input, output } = options
   const dirs = fs.readdirSync(input).reduce<Record<string, any>>((res, name) => (res[name] = 0, res), {})
 
   // 文章
-  const docs = glob.sync(normalizePath(path.join(input, '**/*.md'))).map(filepath => {
+  const articles = glob.sync(normalizePath(path.join(input, '**/*.md'))).map(filepath => {
     const buffer = fs.readFileSync(filepath)
-    const { data } = matter(buffer)
+    const data = matter(buffer).data as { date: string }
     const url = normalizePath(filepath.replace(input, ''))
     const [, category] = url.split('/')
     dirs[category]++
     return { ...data, url, category }
   })
-  // .sort((x, y) => {
-    
-  // })
+  .sort((x, y) => {
+    return new Date(x.date).getTime() - new Date(y.date).getTime()
+  })
 
   // 目录
   const categories = Object.entries(dirs).map(([name, number]) => ({ name, number }))
 
-  console.log('docs => ', docs)
+  // gen json
+  const genJson = (filename: string, data: any) => {
+    fs.writeFileSync(path.join(output, filename), JSON.stringify({ code: 0, data }, null, 2))
+  }
+  fs.ensureDirSync(output)
+  genJson('articles.json', articles)
+  genJson('categories.json', { total: categories.length, list: categories })
 }
 
-run(resolve('articles'))
+run({
+  input: resolve('articles'),
+  output: resolve('mocks'),
+})
