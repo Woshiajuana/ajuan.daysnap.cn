2b61b9191c06039ab4de393c23995fe358ac2351
chenzhigang <979703986@qq.com>
2023-04-20 22:48:44 +0800

    fix: issues 40687

--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -1,4 +1,11 @@
 // https://github.com/vercel/next.js/issues/40687
 module.exports = {
   extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
+  ignorePatterns: ['node_modules', 'dist'],
+  // https://github.com/vercel/next.js/issues/40687
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve('next/babel')],
+    },
+  },
 }
