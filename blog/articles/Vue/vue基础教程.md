bb2fe5f4b53fa5849aead6cf9ad376c5d477069c
chenzhigang <979703986@qq.com>
2023-04-25 22:47:44 +0800

    chore: 新增 markdown-it 插件

--- blog/articles/Vue/vue基础教程.md
+++ blog/articles/Vue/vue基础教程.md
@@ -4,6 +4,8 @@ date: 2020-05-12 12:22
 abstract: 使用了 `ES5` 的 `Object.definePropery` 重写所有属性的 `getter` 和 `setter` 方法；
 ---
 
+[[toc]]
+
 ## 响应式
 
 ![xx](/assets/images/icon-6.jpg)
--- website/src/utils/markdown.ts
+++ website/src/utils/markdown.ts
@@ -1,6 +1,7 @@
 import MarkdownIt from 'markdown-it'
 import hljs from 'highlight.js'
-import anchor from 'markdown-it-anchor'
+import anchorPlugin from 'markdown-it-anchor'
+import tocPlugin from 'markdown-it-table-of-contents'
 import uslug from 'uslug'
 
 const uslugify = (s: string) => uslug(s)
@@ -11,16 +12,28 @@ export const markdown = new MarkdownIt({
   linkify: true,
   typographer: true,
   highlight(str, lang) {
+    // https://github.com/highlightjs/highlight.js/issues/2277
     if (lang && hljs.getLanguage(lang)) {
-      // https://github.com/highlightjs/highlight.js/issues/2277
-      return hljs.highlight(str, { language: lang, ignoreIllegals: true }).value
+      const { value } = hljs.highlight(str, {
+        language: lang,
+        ignoreIllegals: true,
+      })
+      const html = value
+        .split(/\n/)
+        .map((item, index) => {
+          return `<div data-line="${index}">${item}</div>`
+        })
+        .join('')
+      return `<pre>${html}</pre>`
     }
 
     return ''
   },
-}).use(anchor, {
-  slugify: uslugify,
-  permalink: true,
-  permalinkBefore: true,
-  permalinkSymbol: '',
 })
+  .use(anchorPlugin, {
+    slugify: uslugify,
+    permalink: true,
+    permalinkBefore: true,
+    permalinkSymbol: '',
+  })
+  .use(tocPlugin)
