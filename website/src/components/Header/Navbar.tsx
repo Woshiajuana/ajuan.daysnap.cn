1ffd4b2fc9cb9c2625714fb904658c6215a52615
chenzhigang <979703986@qq.com>
2023-05-03 21:18:27 +0800

    feat: navbar

--- website/src/components/Header/Navbar.tsx
+++ website/src/components/Header/Navbar.tsx
@@ -1,12 +1,28 @@
 import Link from 'next/link'
 
+function NavbarLink({ children, href }: { children: any; href: string }) {
+  return (
+    <Link
+      className="relative py-[5px] px-[10px] transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle"
+      href={href}
+    >
+      {children}
+      <div
+        className="absolute inset-0 bg-neutral-100 dark:bg-neutral-800 rounded-md z-[-1]"
+        data-projection-id="1"
+        style={{ transform: 'none', transformOrigin: '50% 50% 0px' }}
+      ></div>
+    </Link>
+  )
+}
+
 export function Navbar() {
   return (
-    <nav>
-      <Link href="/">首页</Link>
-      <Link href="/blog">文章</Link>
-      <Link href="/blog">项目</Link>
-      <Link href="/about">关于</Link>
+    <nav className="flex items-center">
+      <NavbarLink href="/">首页</NavbarLink>
+      <NavbarLink href="/blog">文章</NavbarLink>
+      <NavbarLink href="/blog">项目</NavbarLink>
+      <NavbarLink href="/about">关于</NavbarLink>
     </nav>
   )
 }
--- website/src/utils/constants.ts
+++ website/src/utils/constants.ts
@@ -5,4 +5,5 @@ export const websiteMetadata = {
   description: '分享热门技术以及框架，记录全栈工程师学习成长历程。',
   filing: '湘ICP备2022023602号-1',
   filingUrl: 'https://beian.miit.gov.cn/',
+  githubUrl: '',
 }
