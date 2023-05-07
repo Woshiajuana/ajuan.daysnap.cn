9a3ab5746e08e2733650ffb39a7c96f167f10345
chenzhigang <979703986@qq.com>
2023-05-07 22:47:59 +0800

    chore: article

--- website/src/components/ArticleContent/index.tsx
+++ website/src/components/ArticleContent/index.tsx
@@ -50,7 +50,7 @@ export function ArticleContent(props: ArticleContentProps) {
   return (
     <div
       // className="prose prose-neutral dark:prose-invert"
-      className="prose prose-neutral dark:prose-invert max-w-none prose-headings:font-normal prose-h2:mb-4 prose-h2:mt-8 prose-h2:text-3xl prose-h3:mb-4 prose-h3:mt-8 prose-h3:text-xl prose-h4:mb-4 prose-h4:mt-8 prose-h4:text-xl prose-p:my-4 prose-strong:font-medium prose-strong:text-neutral-200"
+      className="prose prose-neutral dark:prose-invert max-w-none prose-headings:font-normal prose-h2:mb-4 prose-h2:mt-8 prose-h2:text-3xl prose-h3:mb-4 prose-h3:mt-8 prose-h3:text-xl prose-h4:mb-4 prose-h4:mt-8 prose-h4:text-xl prose-p:my-4 prose-strong:font-medium"
       id="article-content"
       dangerouslySetInnerHTML={{
         __html: html,
